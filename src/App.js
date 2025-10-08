7238d387ed5bd0233245fb4e0004629da67ef14e
Vishwajeet Karthikeyan <vishwajeetkarthikeyan@Vishwajeets-Air.lan>
2025-10-08 02:28:54 -0400

    Restore full TuskCPA landing page for Vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import HowItWorks from './components/HowItWorks';
+import Features from './components/Features';
+import Integrations from './components/Integrations';
+import Signup from './components/Signup';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello TuskCPA!</h1>
-      <p>If you can see this, React is working!</p>
+      <Header />
+      <main>
+        <Hero />
+        <HowItWorks />
+        <Features />
+        <Integrations />
+        <Signup />
+      </main>
+      <Footer />
     </div>
   );
 }
